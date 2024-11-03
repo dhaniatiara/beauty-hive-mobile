c011d1c12299915c5728b0479314f6f30b142a1c
dhaniatiara <dhania.t.herdiani@gmail.com>
2024-11-03 23:57:42 +0700

    update menu dart code

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,103 +1,158 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatelessWidget {
+  final String npm = '2306165881'; // NPM
+  final String name = 'Dhania'; // Nama
+  final String className = 'PBP B'; // Kelas
 
-  final String title;
+  MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final List<ItemHomepage> items = [
+    ItemHomepage("Lihat Daftar Produk", Icons.list, const Color(0xFFF4F478)),
+    ItemHomepage("Tambah Produk", Icons.add, const Color(0xFFED6E4B)),
+    ItemHomepage("Logout", Icons.logout, const Color(0xFFDBC1FE)),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title), 
-        backgroundColor: const Color.fromARGB(255, 125, 197, 221), 
+        title: const Text(
+          'Beauty Hive',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
-              'You have pushed the button this many times:',
-              style: TextStyle(fontSize: 18),
+            // Row untuk menampilkan InfoCard
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InfoCard(title: 'NPM', content: npm),
+                InfoCard(title: 'Name', content: name),
+                InfoCard(title: 'Class', content: className),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 40),
+            const SizedBox(height: 16.0),
 
-            // Tombol Lihat Daftar Produk
-            ElevatedButton.icon(
-              onPressed: () {
-                ScaffoldMessenger.of(context)
-                  ..hideCurrentSnackBar()
-                  ..showSnackBar(
-                    const SnackBar(content: Text("Kamu telah menekan tombol Lihat Daftar Produk")),
-                  );
-              },
-              icon: const Icon(Icons.list),
-              label: const Text('Lihat Daftar Produk'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFF4F478), 
+            // Teks Sambutan
+            const Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: Text(
+                'Welcome to Beauty Hive',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
               ),
             ),
-            const SizedBox(height: 10),
 
-            // Tombol Tambah Produk
-            ElevatedButton.icon(
-              onPressed: () {
-                ScaffoldMessenger.of(context)
-                  ..hideCurrentSnackBar()
-                  ..showSnackBar(
-                    const SnackBar(content: Text("Kamu telah menekan tombol Tambah Produk")),
-                  );
-              },
-              icon: const Icon(Icons.add),
-              label: const Text('Tambah Produk'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFED6E4B), 
-              ),
-            ),
-            const SizedBox(height: 10),
-
-            // Tombol Logout
-            ElevatedButton.icon(
-              onPressed: () {
-                ScaffoldMessenger.of(context)
-                  ..hideCurrentSnackBar()
-                  ..showSnackBar(
-                    const SnackBar(content: Text("Kamu telah menekan tombol Logout")),
-                  );
-              },
-              icon: const Icon(Icons.logout),
-              label: const Text('Logout'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFDBC1FE), 
+            // GridView untuk menampilkan tombol dalam bentuk grid
+            Expanded(
+              child: GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                children: items.map((item) {
+                  return ItemCard(item);
+                }).toList(),
               ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  final String title;
+  final String content;
+
+  const InfoCard({super.key, required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2.0,
+      child: Container(
+        width: MediaQuery.of(context).size.width / 3.5,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8.0),
+            Text(content),
+          ],
+        ),
       ),
     );
   }
 }
 
+class ItemHomepage {
+  final String name;
+  final IconData icon;
+  final Color color;
 
+  ItemHomepage(this.name, this.icon, this.color);
+}
+
+class ItemCard extends StatelessWidget {
+  final ItemHomepage item;
+
+  const ItemCard(this.item, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: item.color,
+      borderRadius: BorderRadius.circular(12),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(12),
+        onTap: () {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
+            );
+        },
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.black, // Ubah warna ikon menjadi hitam
+                  size: 30.0,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.black), // Ubah warna teks menjadi hitam
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
